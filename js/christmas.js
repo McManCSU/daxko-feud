25f0feb4e4c9914a5d6bc96a7bd69495fe40ce40
Ken Michie <ken@fullcontact.com>
2017-12-20 21:11:03 -0700

    Christmas Questions

---
+++ js/christmas.js
@@ -0,0 +1,180 @@
+var data = [
+["Name one of Santa's reindeers.",
+  [
+    'Rudolph', 50,
+    'Dasher', 20,
+    'Dancer', 09,
+    'Blixen', 07,
+    'Prancer', 06,
+    'Donner', 04,
+    'Vixen', 02
+  ]],
+["Name something you need to wrap a gift.",
+  [
+    'Tape', 55,
+    'Wrapping Paper', 34,
+    'Gifts', 04,
+    'Ribbon', 03,
+    'Box', 02
+  ]],
+["What is a popular holiday greeting?",
+  [
+    'Merry Christmas', 78,
+    'Happy Holidays', 17,
+    'Seasons Greetings', 03,
+    'Feliz Navidad', 02
+  ]],
+["Who is the most popular person associated with Christmas?",
+  [
+    'Santa', 74,
+    'Jesus/Christ', 24,
+    'Mary', 02
+  ]],
+["Name one food you might have at a Christmas dinner.",
+  [
+    'Turkey', 63,
+    'Ham', 25,
+    'Mashed Potatoes', 04,
+    'Stuffing', 03,
+    'Pie', 03,
+    'Cranberries', 02
+  ]],
+["Name a drink that might be served at a holiday party.",
+  [
+    'Eggnog', 71,
+    'Punch', 08,
+    'Cider/Hot Apple Cider', 08,
+    'Wine', 03,
+    'Wassail', 02,
+    'Coffee', 02
+  ]],
+["If you wanted to dress up like Santa, name one thing you would need.",
+  [
+    'Beard', 40,
+    'Hat/Red Hat/Santa Hat', 27,
+    'Red Suit/Santa Suit', 19,
+    'Boots/Black Boots', 06,
+    'Big Belly/Fat Stomach', 03,
+    'Belt', 03,
+    'Pillow', 02
+  ]],
+["Name a word or phrase that begins with 'snow'.",
+  [
+    'Snowflake(s)', 30,
+    'Snowman', 26,
+    'Snowball', 23,
+    'Snow White', 08,
+    'Snow Angel', 03,
+    'Snowfall', 03
+  ]],
+["Name one color associated with Christmas.",
+  [
+    'Red', 77,
+    'Green', 16,
+    'White', 05,
+    'Gold', 02
+  ]],
+["Name one piece of a nativity set.",
+  [
+    'Baby Jesus', 56,
+    'Manger', 23,
+    'Wiseman', 05,
+    'Mary', 04,
+    'Angel', 03,
+    'Sheep/Lamb', 03,
+    'Donkey', 03
+  ]],
+["What is a popular Christmas tree topper?",
+  [
+    'Star', 61,
+    'Angel', 35,
+    'Santa', 04
+  ]],
+["Name a popular Chistmas carol featuring a fictional character.",
+  [
+    'Rudolph the Red-Nosed Reindeer', 50,
+    'Frosty the Snowman', 35,
+    'Santa Claus is Coming to Town', 05,
+    'Good King Wencelas', 03,
+    'The Little Drummer Boy', 02
+  ]],
+["Name a Christmas movie featuring Santa.",
+  [
+    'The Santa Clause (I, II and III)', 41,
+    'Miracle on 34th Street', 28,
+    'Rudolph the Red-Nosed Reindeer', 06,
+    'Elf', 05,
+    'Santa Claus is Coming to Town', 03,
+    'A Christmas Story', 02,
+    'Frosty the Snowman', 02,
+    'Christmas Vacation', 02,
+    'The Night Before Christmas', 02
+  ]],
+["Name a popular Christmas symbol.",
+  [
+    'Christmas Tree/Tree', 39,
+    'Star', 34,
+    'Santa', 04,
+    'Bells', 03,
+    'Cross', 03,
+    'Wreath', 03,
+    'Holly', 03,
+    'Manger', 02,
+    'Angel', 02,
+    'Snowflake', 02,
+    'Candy Cane', 02,
+    'Ornament', 02
+  ]],
+["Name something people do on Christmas day.",
+  [
+    'Open gifts', 72,
+    'Eat', 17,
+    'Go to church', 06,
+    'Sing carols', 03,
+    'Visit family', 02
+  ]],
+["Name a Christmas story parents read their children.",
+  [
+    'Twas the Night Before Christmas', 77,
+    'The Birth of Jesus/Luke 2', 08,
+    'Rudolph the Red-Nosed Reindeer', 04,
+    'The Nutcracker', 02,
+    'Frosty the Snowman', 02,
+    'A Christmas Carol', 02,
+    'How the Grinch Stole Christmas', 02
+  ]],
+["Name a Christmas plant.",
+  [
+    'Poinsettia', 71,
+    'Christmas Cactus', 08,
+    'Christmas Tree', 06,
+    'Mistletoe', 06,
+    'Holly', 06
+  ]],
+["Name something you might find at the North Pole.",
+  [
+    'Snow', 34,
+    'Elves', 22,
+    'Santa Claus', 16,
+    'Santas Toyshop/Santas Workshop', 09,
+    'Reindeer', 05,
+    'Ice', 04,
+    'Mrs. Claus', 02,
+    'Igloo', 02,
+    'Toys', 02
+  ]],
+["Name a Christmas song with the word 'night' in the title.",
+  [
+    'Silent Night', 81,
+    'O Holy Night', 15,
+    'The Night Before Christmas', 04
+  ]],
+["Name a Christmas cartoon that plays on TV every year.",
+  [
+    'Rudolph the Red-Nosed Reindeer', 29,
+    'A Charlie Brown Christmas', 26,
+    'Frosty the Snowman', 19,
+    'How the Grinch Stole Christmas', 17,
+    'Santa Claus is Coming to Town', 03
+  ]]
+];
